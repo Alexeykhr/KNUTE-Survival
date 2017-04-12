a622859ef458bd1be11864776c8f08831a1704f8
Alexey Khrusch <alexeykhr@outlook.com>
2017-04-12 20:52:36 +0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # KNUTE-Survival
+
+DB docs - https://github.com/usmanhalalit/pixie
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kadey
- * Date: 12.04.2017
- * Time: 20:41
- */
+
+require_once __DIR__ . 'vendor/autoload.php';
+
+
+// Docs: https://github.com/usmanhalalit/pixie
+
+//$config = array(
+//    'driver'    => 'mysql', // Db driver
+//    'host'      => 'localhost',
+//    'database'  => 'your-database',
+//    'username'  => 'root',
+//    'password'  => 'your-password',
+//    'charset'   => 'utf8', // Optional
+//    'collation' => 'utf8_unicode_ci', // Optional
+//    'prefix'    => 'cb_', // Table prefix, optional
+//);
+
+//new \Pixie\Connection('mysql', $config, 'QB');
