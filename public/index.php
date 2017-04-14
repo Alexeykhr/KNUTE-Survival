9f59b73d92e0b8dd6f988a87ce84771b8d4f9963
Alexey Khrusch <alexeykhr@outlook.com>
2017-04-14 16:46:38 +0300

    Random byte generator

--- public/index.php
+++ public/index.php
@@ -2,7 +2,10 @@
 
 define('KNUTE_DIR', __DIR__ . '/../');
 
-require KNUTE_DIR . '/src/view/login/index.html';
+//require KNUTE_DIR . '/src/view/login/index.html';
+
+// Для генерации рандомных символов
+// $random = $login . ',' . bin2hex( openssl_random_pseudo_bytes(15) );
 
 // Если пользователь не авторизован => показываем авторизацию/регистрацию
 // Если пользователь авторизован    => показываем контент
